c2e0485c5a5811e2f3bcc60d72843f42c36be772
Sunshine-wzy <sunshinewzy@qq.com>
2023-03-21 20:06:12 +0800

    Add library algs4.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,18 @@ repositories {
 }
 
 dependencies {
+    implementation(fileTree("libs"))
+    
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
 }
--- src/test/java/io/github/sunshinewzy/algorithms/fundamentals/programming_model/ArrayTest.java
+++ src/test/java/io/github/sunshinewzy/algorithms/fundamentals/programming_model/ArrayTest.java
@@ -1,5 +1,6 @@
 package io.github.sunshinewzy.algorithms.fundamentals.programming_model;
 
+import edu.princeton.cs.algs4.StdOut;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,8 +12,8 @@ public class ArrayTest {
 		int[] a = {1, 2, 3, 4, 5};
 		int[] b = new int[]{6, 7, 8, 9, 10};
 
-		System.out.println(Arrays.toString(a));
-		System.out.println(Arrays.toString(b));
+		StdOut.println(Arrays.toString(a));
+		StdOut.println(Arrays.toString(b));
 	}
 	
 	@Test
@@ -20,13 +21,13 @@ public class ArrayTest {
 		String[] a = {"a", "b", "c"};
 		String[] b = new String[]{"d", "e", "f"};
 
-		System.out.println(Arrays.toString(a));
+		StdOut.println(Arrays.toString(a));
 		string(b);
 		string(new String[]{"g", "h", "i"});
 	}
 	
 	private void string(String[] array) {
-		System.out.println(Arrays.toString(array));
+		StdOut.println(Arrays.toString(array));
 	}
 	
 }
